420a8e118f44916a2cba17bbfc93bdf1178f9079
josevinoth <101917197+josevinoth@users.noreply.github.com>
2024-08-15 07:06:00 +0530

    First commit-Jose (15-Aug-24) Urls

--- tt_project/tt_app/urls.py
+++ tt_project/tt_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views #import this
 urlpatterns = [
     path('cruise', views.cruise_view, name='cruise'),  # access cruise
     path('home_page', views.home_page_view, name='home_page'),  # access cruise
