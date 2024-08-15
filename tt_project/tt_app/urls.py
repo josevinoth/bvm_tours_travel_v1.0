ddbf9e6cb0b3c391ee69dd0b83cbc78d101088f4
poojauday <23pooji@mail.com>
2024-08-15 21:48:16 +0530

    commit pooja(15-aug-2024)

--- tt_project/tt_app/urls.py
+++ tt_project/tt_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views #import this
 urlpatterns = [
+    path('wedding_india', views.wedding_india_view, name='wedding_india'),
+    path('wedding_goa', views.wedding_goa_view, name='wedding_goa'),
     path('cruise', views.cruise_view, name='cruise'),  # access cruise
-    path('home_page', views.home_page_view, name='home_page'),  # access cruise
+    path('home_page', views.home_page_view, name='home_page'),
+
 ]
